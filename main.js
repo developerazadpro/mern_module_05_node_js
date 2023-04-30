4b2daa3b94ca3fe54c76d192329316b3e18ccac1
developerazadpro <azharul.ece13.hstu@gmail.com>
2023-05-01 00:22:47 +0600

    request response model in node js

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 let http = require('http');
 
 let server = http.createServer(function (req, res){
-    res.end("Hello World");
+    if(req.url == '/'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write('<h1>This is Home Page<h1/>')
+        res.end();
+    }else if(req.url == '/about'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write('<h1>This is About Page<h1/>')
+        res.end();
+    }else if(req.url == '/contact'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write('<h1>This is Contact Page<h1/>')
+        res.end()
+    }else{
+        res.writeHead(404, {'Content-Type': 'text/html'})
+        res.write('<h1>This is 404 Page<h1/>')
+        res.end()
+    }
 });
 
 server.listen(5050);
