993f12664a1ef9949fbccb8443ab309781d13941
developerazadpro <azharul.ece13.hstu@gmail.com>
2023-05-01 20:19:06 +0600

    fs file write assycn

--- fsModule/index.js
+++ fsModule/index.js
@@ -15,6 +15,19 @@ let server = http.createServer(function (req, res){
         res.writeHead(200, {'Content-Type': 'text/html'})
         res.write(data)
         res.end();
+    }else if(req.url == '/write-file'){
+        // fs fil write Assync
+        fs.writeFile('demo.txt', 'Welcome to node js', function (error){
+            if(error){
+                res.writeHead(200, {'Content-Type': 'text/html'})
+                res.write('File write failed')
+                res.end();
+            }else{
+                res.writeHead(200, {'Content-Type': 'text/html'})
+                res.write('File write success')
+                res.end();
+            }
+        })
     }
 })
 
